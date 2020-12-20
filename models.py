65946b564272ecdaebbb6d0f1e684c3c6f5e1e2a
ericlearning <48952432+ericlearning@users.noreply.github.com>
2020-12-21 02:48:18 +0900

    bug fixes and ResNet loader

--- models.py
+++ models.py
@@ -3,13 +3,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class BasicBlock(nn.Module):
+    expansion = 1
     def __init__(self, ic, oc, stride, dilation):
         super(BasicBlock, self).__init__()
-        self.expansion = 1
         self.conv1 = nn.Conv2d(ic, oc, 3, stride, dilation, dilation, bias=False)
         self.bn1 = nn.BatchNorm2d(oc)
-        self.conv2 = nn.Conv2d(oc, oc, 3, 1, 1, bias=False)
-        self.bn2 = nn.BatchNorm2d(oc)
+        self.conv2 = nn.Conv2d(oc, oc * self.expansion, 3, 1, 1, bias=False)
+        self.bn2 = nn.BatchNorm2d(oc * self.expansion)
         
         if stride != 1 or ic != oc * self.expansion:
             self.shortcut = nn.Sequential(
@@ -30,9 +30,9 @@ class BasicBlock(nn.Module):
         return out
 
 class Bottleneck(nn.Module):
+    expansion = 4
     def __init__(self, ic, oc, stride, dilation):
         super(Bottleneck, self).__init__()
-        self.expansion = 4
         self.conv1 = nn.Conv2d(ic, oc, 1, 1, 0, bias=False)
         self.bn1 = nn.BatchNorm2d(oc)
         self.conv2 = nn.Conv2d(oc, oc, 3, stride, dilation, dilation, bias=False)
@@ -69,6 +69,7 @@ class Flatten(nn.Module):
 
 class ResNet(nn.Module):
     def __init__(self, ic, oc, block, num_blocks, strides=[1, 2, 2, 2], use_dilations=[False, False, False]):
+        super(ResNet, self).__init__()
         self.conv1 = nn.Conv2d(ic, 64, 7, 2, 3, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.maxpool1 = nn.MaxPool2d(3, 2, 1)
@@ -76,16 +77,25 @@ class ResNet(nn.Module):
         self.dilation = 1
 
         expansion = block.expansion
-        self.layer1 = self.make_layer(num_blocks[0], block, ic, 64, strides[0], False)
-        self.layer2 = self.make_layer(num_blocks[1], block, 64 * expansion, 128, strides[1], dilations[1])
-        self.layer3 = self.make_layer(num_blocks[2], block, 128 * expansion, 256, strides[2], dilations[2])
-        self.layer4 = self.make_layer(num_blocks[3], block, 256 * expansion, 512, strides[3], dilations[3])
+        self.layer1 = self.make_layer(num_blocks[0], block, 64, 64, strides[0], False)
+        self.layer2 = self.make_layer(num_blocks[1], block, 64 * expansion, 128, strides[1], use_dilations[0])
+        self.layer3 = self.make_layer(num_blocks[2], block, 128 * expansion, 256, strides[2], use_dilations[1])
+        self.layer4 = self.make_layer(num_blocks[3], block, 256 * expansion, 512, strides[3], use_dilations[2])
         
         self.cls = nn.Sequential(
             nn.AdaptiveAvgPool2d((1, 1)),
             Flatten(),
             nn.Linear(512 * expansion, oc)
         )
+        self.initialize_network()
+    
+    def initialize_network(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1.0)
+                nn.init.constant_(m.bias, 0.0)
     
     def make_layer(self, num_blocks, block, ic, oc, stride, use_dilation):
         dilation = self.dilation
@@ -96,7 +106,7 @@ class ResNet(nn.Module):
         layer = [block(ic, oc, stride, dilation)]
         for _ in range(num_blocks-1):
             layer.append(block(oc * block.expansion, oc, 1, self.dilation))
-        return nn.Sequential(*layers)
+        return nn.Sequential(*layer)
     
     def forward(self, x):
         out = self.conv1(x)
@@ -111,3 +121,16 @@ class ResNet(nn.Module):
         
         out = self.cls(out)
         return out
+
+
+def get_resnet(ic, oc, resnet_type):
+    if resnet_type == "resnet18":
+        return ResNet(ic, oc, BasicBlock, [2, 2, 2, 2], [1, 2, 2, 2], [False, False, False])
+    elif resnet_type == "resnet34":
+        return ResNet(ic, oc, BasicBlock, [3, 4, 6, 3], [1, 2, 2, 2], [False, False, False])
+    elif resnet_type == "resnet50":
+        return ResNet(ic, oc, Bottleneck, [3, 4, 6, 3], [1, 2, 2, 2], [False, False, False])
+    elif resnet_type == "resnet101":
+        return ResNet(ic, oc, Bottleneck, [3, 4, 23, 3], [1, 2, 2, 2], [False, False, False])
+    elif resnet_type == "resnet152":
+        return ResNet(ic, oc, Bottleneck, [3, 8, 36, 3], [1, 2, 2, 2], [False, False, False])
